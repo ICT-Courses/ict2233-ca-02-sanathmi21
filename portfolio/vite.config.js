00796567e520b5c375a6bc4f9fa97f2a86e0eba8
sanathmi21 <ar112759@fhss.sjp.ac.lk>
2025-09-24 02:18:47 +0530

    delete unnecessary files

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/ict2233-ca-02-sanathmi21/',
   plugins: [react(), tailwindcss()],
